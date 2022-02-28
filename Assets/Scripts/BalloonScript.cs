4cc3bada304a7d0f89b118394ebcb503ca2f75af
Oğuzhan Şen <48945284+oguzhansen@users.noreply.github.com>
2022-02-28 20:13:38 +0300

    Initial commit

---
+++ Assets/Scripts/BalloonScript.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BalloonScript : MonoBehaviour {
+
+	public List<Sprite> balloonImages;
+	public Sprite deadlyBalloonImage;
+
+	private GameControllerScript gcScript;
+
+	public bool isActive = false;
+	public bool isDeadly = false;
+
+	// Use this for initialization
+	void Start () {
+		ResetPosition();
+		gcScript = this.transform.parent.GetComponent<GameControllerScript>();
+	}
+
+	public void MakeDeadly() {
+		isDeadly = true;
+		this.GetComponent<SpriteRenderer>().sprite = deadlyBalloonImage;
+	}
+
+	public void MakeFriendly() {
+		isDeadly = false;
+		int balloonChoice = Random.Range (0, balloonImages.Count);
+		this.GetComponent<SpriteRenderer>().sprite = balloonImages[balloonChoice];
+	}
+
+	void ResetPosition() {
+		this.transform.position = new Vector3(0.0f, -10.0f, 0.0f);
+	}
+
+	void Update() {
+		if (this.transform.position.y > 6.0f && isActive) {
+			Deactivate();
+		}
+	}
+
+	public void Activate() {
+		isActive = true;
+		float upSpeed = Random.Range (1.5f, 4.0f);
+		this.GetComponent<Rigidbody2D>().velocity = new Vector3(0.0f, upSpeed, 0.0f);
+		this.transform.position = new Vector3(Random.Range (-2.4f, 2.45f), -6.0f, 0.0f);
+		if (Random.Range(0,4) == 0) {
+			MakeDeadly();
+		} else {
+			MakeFriendly ();
+		}
+	}
+
+	public void Deactivate() {
+		isActive = false;
+		this.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
+		ResetPosition();
+	}
+
+	void OnMouseDown() {
+		Pop ();
+	}
+
+	public void Pop() {
+		if (isDeadly) {
+			gcScript.GameOver();
+		} else {
+			gcScript.AddPoints(1);
+			Deactivate ();
+		}
+	}
+}
---
+++ Assets/Scripts/GameControllerScript.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GameControllerScript : MonoBehaviour {
+
+	public Transform balloonPrefab;
+	public Text      scoreDisplay;
+
+	public SpriteRenderer surprise;
+	public List<Sprite> items;
+	public List<Sprite> item;
+	private int _surpriseItem;
+	private int _surpriseRandom;
+
+	private int             _playerScore        = 0;
+	private int             _multiplier         = 1;
+	private float           _timeSinceLastSpawn = 0.0f;
+	private float           _timeToSpawn        = 0.0f;
+	private List<Transform> _balloons;
+
+	private const int BALLOON_POOL = 35;
+
+	void Start () {
+
+		//Ödül Kısmı
+
+		surprise.enabled = false;
+		_surpriseRandom = Random.Range(1, 10);
+		_surpriseItem = Random.Range(0, 9);
+
+		//Ödül Kısmı
+
+		_balloons = new List<Transform>();
+		for (int i = 0; i < BALLOON_POOL; i++) {
+			Transform balloon = Instantiate(balloonPrefab) as Transform;
+			balloon.parent = this.transform;
+			_balloons.Add(balloon);
+		}
+		SpawnBalloon();
+
+		GameStart();
+	}
+
+	void InitMultiplier() {
+		if (PlayerPrefs.HasKey("Multiplier")) {
+			_multiplier = Mathf.Max (1, PlayerPrefs.GetInt ("Multiplier"));
+		}
+	}
+
+	void InitPoints() {
+		if (PlayerPrefs.HasKey("Points")) {
+			_playerScore = PlayerPrefs.GetInt("Points");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Ödül Kısmı
+		if (_surpriseRandom == _playerScore)
+		{
+			_playerScore += 1;
+			surprise.enabled = true;
+			if (surprise.enabled == true)
+			{
+				items.Add(item[_surpriseItem]);
+			}
+			StartCoroutine(CoroutineTest());
+		}
+
+		IEnumerator CoroutineTest()
+		{
+			yield return new WaitForSeconds(0.1f);
+			surprise.enabled = false;
+		}
+
+		//Ödül Kısmı
+
+		_timeSinceLastSpawn += Time.deltaTime;
+		if (_timeSinceLastSpawn >= _timeToSpawn) {
+			SpawnBalloon();
+		}
+	}
+
+	void SpawnBalloon() {
+		_timeSinceLastSpawn = 0.0f;
+		_timeToSpawn = Random.Range (0.0f, 2.0f);
+		foreach (Transform b in _balloons) {
+			BalloonScript bs = b.GetComponent<BalloonScript>();
+			if (bs && !bs.isActive) {
+				bs.Activate();
+				break;
+			}
+		}
+	}
+
+	public void AddPoints(int points=1) {
+		_playerScore += points;
+		UpdateScoreDisplay();
+	}
+
+	public void GameOver() {
+		SavePoints();
+		Application.LoadLevel("TitleScreen");
+	}
+
+	void UpdateScoreDisplay() {
+		scoreDisplay.text = "Score: " + _playerScore.ToString() + "(x" + _multiplier.ToString() + ")";
+	}
+
+	public void GameStart() {
+		InitPoints();
+		InitMultiplier();
+		UpdateScoreDisplay();
+	}
+
+	void OnApplicationPause() {
+		SavePoints();
+	}
+
+	void OnApplicationQuit() {
+		SavePoints();
+	}
+
+	void SavePoints() {
+		PlayerPrefs.SetInt("Points", _playerScore);
+	}
+}
